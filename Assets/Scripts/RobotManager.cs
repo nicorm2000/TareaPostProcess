435be79de39cbb72f7c4250adfd8b3f182d6bc2a
Nicolas Ramos Marin <nicorm2000@gmail.com>
2021-12-11 17:45:13 -0300

    1

---
+++ Assets/Scripts/RobotManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotManager : MonoBehaviour
+{
+    [SerializeField] float mouseXAxis;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(speed);
+        RotatePlayer();
+    }
+    private void Move(float speed)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+    }
+
+    private void RotatePlayer()
+    {
+        mouseXAxis += Input.GetAxis("Mouse X");
+        Quaternion newrotation = Quaternion.Euler(0, mouseXAxis, 0);
+        transform.rotation = newrotation;
+    }
+}
